7faf1703a74556afc9b8d3597482009d49945c6f
Ryan <ryan.deline@gmail.com>
2021-05-06 09:00:38 -0400

    test coded join

--- Code/Clean_1.py
+++ Code/Clean_1.py
@@ -12,15 +12,21 @@ import pandas as pd
 import numpy as np 
 from tqdm import tqdm
 
-winfile = "Y:/GitHub/Indiana-Bridges/NBI_Files/NBI_****.csv"
+winfile = "Y:/GitHub/Indiana-Bridges/NBI_Files/NBI_2010.csv"
 ### Database Connection
 woutfile = "Y:/GitHub/Indiana-Bridges/NBI_Files/Clean/NBI_****.csv"
-codefile = 
+codefile = "Y:/GitHub/Indiana-Bridges/Column_Tables/Coding.csv"
 
 df = pd.read_csv(winfile, Low_memory = False, encoding = "ISO-8859-1")
+df2 = pd.read_csv(codefile)
 
 ### Drop columns which are not consistant year to year.
 df = df.drop(['FED_AGENCY', 'DATE_LAST_UPDATE', 'TYPE_LAST_UPDATE', 'DEDUCT_CODE',	'REMARKS', 'PROGRAM_CODE',
 					'PROJ_NO', 'PROJ_SUFFIX', 'NBI_TYPE_OF_IMP', 'DTL_TYPE_OF_IMP',	'STEP_CODE', 'STATUS_WITH_10YR_RULE',
 					'SUFFICIENCY_ASTERC', 'SUFFICIENCY_RATING',	'STATUS_NO_10YR_RULE'])
 
+df3 = pd.concat([df, df2], axis = 1)
+
+print(df3)
+
+df3.to_csv(woutfile, sep = ',')
