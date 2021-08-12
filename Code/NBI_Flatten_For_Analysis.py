d4eb8bda337ad2921bdd40778549ce60621747f3
Ryan <ryan.deline@gmail.com>
2021-08-12 15:17:13 -0400

    Updating Fields with Descriptions

--- Code/NBI_Flatten_For_Analysis.py
+++ Code/NBI_Flatten_For_Analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import numpy as np
+import re
 import pyodbc
 from tqdm import tqdm
 
@@ -18,6 +19,10 @@ cnxn = pyodbc.connect(conn_str)
 
 # cursor = cnxn.cursor()
 sql = """Select * FROM Indiana_Bridges.dbo.NBI"""
+sql2 = """Select * FROM Indiana_Bridges.dbo.FUNCTIONAL_CLASS_026"""
+sql3 = """Select * FROM Indiana_Bridges.dbo.COUNTY_CODE_003"""
+sql4 = """Select * FROM Indiana_Bridges.dbo.MAINTENANCE_021"""
+sql5 = """Select * FROM Indiana_Bridges.dbo.OWNER_022"""
 # cursor.execute(sql)
 
 ### Verify output in terminal
@@ -25,9 +30,13 @@ sql = """Select * FROM Indiana_Bridges.dbo.NBI"""
 # for row in cursor:
 # 	print(row)
 
-### Convert database stream into pandas dataframe
+### Convert database streams into pandas dataframe
+
 df = pd.read_sql(sql, cnxn)
-# print(df.head(5))
+df2 = pd.read_sql(sql2, cnxn)
+df3 = pd.read_sql(sql3, cnxn)
+df4 = pd.read_sql(sql4, cnxn)
+df5 = pd.read_sql(sql5, cnxn)
 
 ### Begining Cleaning Analyst Output
 df = df.drop(['STATE_CODE_001', 'RECORD_TYPE_005A', 'ROUTE_PREFIX_005B', 'SERVICE_LEVEL_005C',
@@ -38,9 +47,32 @@ df = df.drop(['STATE_CODE_001', 'RECORD_TYPE_005A', 'ROUTE_PREFIX_005B', 'SERVIC
 				'LEFT_LAT_UND_MT_056', 'IMP_LEN_MT_076', 'BRIDGE_IMP_COST_094', 'ROADWAY_IMP_COST_095',
 				'OTHER_STATE_CODE_098A', 'OTHER_STATE_PCNT_098B', 'OTHR_STATE_STRUC_NO_099', 
 				'PARALLEL_STRUCTURE_101', 'TRAFFIC_DIRECTION_102', 'PIER_PROTECTION_111', 'BRIDGE_LEN_IND_112',
-				'FUTURE_ADT_114', 'YEAR_OF_FUTURE_ADT_115', 'MIN_NAV_CLR_MT_116', 'DIRECTION_005E', 'Toll_20',
+				'FUTURE_ADT_114', 'YEAR_OF_FUTURE_ADT_115', 'MIN_NAV_CLR_MT_116', 'DIRECTION_005E', 'TOLL_020',
 				], axis=1)
 
+### Function Classification Decoded
+
+df['FUNCTIONAL_CLASS_026'] = df['FUNCTIONAL_CLASS_026'].map(df2.set_index('Code')['Description'])
+
+### County Decoded
+
+df['COUNTY_CODE_003'] = df['COUNTY_CODE_003'].map(df3.set_index('Code')['County'])
+
+### Maintenance Responsibility
+
+df['MAINTENANCE_021'] = df['MAINTENANCE_021'].map(df4.set_index('Code')['Description'])
+
+### Owner
+
+df['OWNER_022'] = df ['OWNER_022'].map(df5.set_index('Code')['Description'])
+
+### Design Load
+
+df['DESIGN_LOAD_031']
+
+### Historical Significance
+
+df['']
 
 df = df.rename(columns = {'STRUCTURE_NUMBER_008':'NBI Number', 'Year':'Rating Year', 'Latitude':'Latitude',
 							'Longitude':'Longitude', 'DATE_OF_INSPECT_090':'Inspection Date', 'INSPECT_FREQ_MONTHS_091':'Inspection Frequency',
@@ -75,6 +107,8 @@ df = df.rename(columns = {'STRUCTURE_NUMBER_008':'NBI Number', 'Year':'Rating Ye
 							'SURFACE_TYPE_108A':'SURFACE_TYPE_108A', 'MEMBRANE_TYPE_108B':'MEMBRANE_TYPE_108B', 'DECK_PROTECTION_108C':'DECK_PROTECTION_108C',
 							'PERCENT_ADT_TRUCK_109':'PERCENT_ADT_TRUCK_109', 'NATIONAL_NETWORK_110':'NATIONAL_NETWORK_110'})
 
+
 ### Output to Master Bridge Analysis Record
 df = df.head(5)
 df.to_csv(woutfile, sep = ',')
+tqdm.pandas()
